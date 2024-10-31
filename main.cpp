3c10079d2326a73a33703689639999b3c5005701
vhezckoslav <vojtech.heczko@gmail.com>
2024-10-31 12:44:59 +0100

    push before re-do

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-typedef enum tokenType{
+typedef enum{
     VARDEC,
     VARDEF,
     OPERATOR,
@@ -16,7 +16,7 @@ typedef enum tokenType{
     INT,
     FLOAT,
     LONG,
-};
+}tokenType;
 
 template <typename T> class Scalar{
 
@@ -30,26 +30,43 @@ template <typename T> class Matrix{
 
 };
 
-struct Maps {
+struct{
     std::unordered_map<std::string, tokenType> dataTypeMap;
     std::unordered_map<std::string, int> intMap;
     std::unordered_map<std::string, float> floatMap;
     std::unordered_map<std::string, long> longMap;
-};
+}Maps;
 
-int vardec(string &token, char c, std::vector<std::regex> RegExes){
+struct{
+    std::regex varNameFirstLetter = std::regex("[A-Za-z]"); 
+    std::regex varNameNonFirstLetter = std::regex("[A-Za-z0-9]");
+    std::regex operators = std::regex("^(\\/=|\\+=|\\-=|\\*=|=)$");
+}RegExes;
+
+void savevar(){}
+
+int vardec(string &token, char c){
     string s(1, c);
     if(token.length() == 0){
-        if(!regex_search(s, RegExes[0])){
+        if(regex_search(s, RegExes.operators)){
+            cout << "You must declare var before assigning value!" << endl;
             return -1;
+        }
+        if(!regex_search(s, RegExes.varNameFirstLetter)){
+            cout << "Incorrect var name format!" << endl;
+            return -2;
         }
         else{
             token += c;
         }
     }
     else{
-        if(!regex_search(s, RegExes[1])){
-            return -1;
+        if(!regex_search(s, RegExes.operators)){
+            return -3;
+        }
+        if(!regex_search(s, RegExes.varNameNonFirstLetter)){
+            cout << "Incorrect var name format or operation!" << endl;
+            return -2;
         }
         else{
             token += c;
@@ -62,33 +79,45 @@ int printVal(string &varToken){
     string normalizedToken;
     int len = varToken.length();
     if(len == 0){
-        return -2;
+        return -1;
     }
     for(int i = 0; i < len; i++){
         if(varToken[i] == ' ') continue;
         else { normalizedToken += varToken[i]; }
     }
+    auto it = Maps.dataTypeMap.find(normalizedToken);
+    if (it == Maps.dataTypeMap.end()) {
+        return -2;
+    }
+
+    switch (it->second) {
+        case tokenType::INT:
+            std::cout << Maps.intMap[normalizedToken] << std::endl;
+            break;
+        case tokenType::FLOAT:
+            std::cout << Maps.floatMap[normalizedToken] << std::endl;
+            break;
+        case tokenType::LONG:
+            std::cout << Maps.longMap[normalizedToken] << std::endl;
+            break;
+        default:
+            return -3;
+    }
     return 0;
 }
 
 int lexer(string &tokensLine){
-    std::vector<std::regex> RegExes = {
-        std::regex("[A-Za-z]"),       
-        std::regex("[A-Za-z0-9]")       
-    };
     int len = tokensLine.length();
     tokenType dataType = INT;
     tokenType currentExpected = VARDEC;
     string currentToken = "";
     int col = 0;
     for(int i = 0; i < len; i++){
-        if(tokensLine[i] == ' ') { continue; }
+        //if(tokensLine[i] == ' ') { continue; }
         if(currentExpected == VARDEC){
-            if(tokensLine[i] == '='){
-                currentExpected = VARDEF;
-                continue;
-            }
-            if(vardec(currentToken, tokensLine[i], std::vector<std::regex>{RegExes[0], RegExes[1]}) == -1) { cout << "Incorrect var name format!" << endl; break; }
+            if(vardec(currentToken, tokensLine[i]) == -3){
+                savevar(currentToken)
+            };
         }
         if(currentExpected = VARDEF){
 
@@ -97,14 +126,14 @@ int lexer(string &tokensLine){
             int response = 1;
             response = printVal(currentToken);
             switch(response){
-                case 0:
-
-                    break;
                 case -1:
 
                     break;
                 case -2:
-                cout << "No variable name provided!" << endl;
+
+                    break;
+                case -3:
+
                     break;
             }
         }
