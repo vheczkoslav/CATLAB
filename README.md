51b9727aa64a08758b24ca33acb8bea982a57c7d
vhezckoslav <vojtech.heczko@gmail.com>
2024-11-01 13:38:55 +0100

    Redo

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ CATLAB (meow) written in C++
 CATLAB is used in the same way as MATLAB: when you turn it on (with ./catlab) there is a terminal that is gonna be running until you press CTRL+C, enter ex command or destroy the program :) I dare you
 ## How to work with CATLAB
 Super basic concept in CATLAB is of course defining variables. Variable can contain only small and big characters of english alphabet and numbers (not in the beginning tho). Declaration can be like this:
-A=120
+A = 120
 B = 100
 This is a scalar number definition
 How to declare vector:
-V=[1,2,3,4,5]
+V = [ 1 2 3 4 5]
 W = [5 4 3 2 1]
 How to declare matrix:
-M={1,1,1;2,2,2;3,3,3}
+M = {1 1 1 ; 2 2 2 ; 3 3 3}
 Of course if the inside sizes of row would not match, you will get an error.
 Default type of any number is INT. However there are two more types: FLOAT and LONG.
 If you wish to type FLOAT just set the value to be in an x.y form i.e. 5.1 for example
--- main.cpp
+++ main.cpp
@@ -6,17 +6,60 @@
 
 using namespace std;
 
-typedef enum{
-    VARDEC,
-    VARDEF,
-    OPERATOR,
-    SCALAR,
-    VECTOR,
-    MATRIX,
-    INT,
-    FLOAT,
-    LONG,
-}tokenType;
+enum TokenType
+{
+    Integer = 11, // or literal
+    Float = 12,
+    Long = 13,
+    Delimiter = 20, // ; for Vector and Matrixes
+    Identifier = 30,
+    Operator = 41,
+    Equals = 42,
+    OpenBracket = 51,
+    CloseBracket = 52,
+    OpenCurly = 53,
+    CloseCurly = 54,
+    Invalid = 99
+};
+
+string tokenTypeIdToString(TokenType tt){
+    switch(tt){
+        case 11:
+            return "Integer";
+        case 12:
+            return "Float";
+        case 13:
+            return "Float";
+        case 20:
+            return "Delimiter";
+        case 30:
+            return "Identifier";
+        case 41:
+            return "Operator";
+        case 42:
+            return "Equals";
+        case 51:
+            return "Open Bracket";
+        case 52:
+            return "Closed Bracket";
+        case 53:
+            return "Open Curly";
+        case 54:
+            return "Closed Curly";
+        case 99:
+            return "Invalid";
+        default:
+            return "Something went really wrong";                 
+    }
+}
+
+typedef TokenType DataType;
+
+struct Token
+{
+    std::string value;
+    TokenType type;
+};
 
 template <typename T> class Scalar{
 
@@ -31,124 +74,108 @@ template <typename T> class Matrix{
 };
 
 struct{
-    std::unordered_map<std::string, tokenType> dataTypeMap;
+    std::unordered_map<std::string, DataType> dataTypeMap;
     std::unordered_map<std::string, int> intMap;
     std::unordered_map<std::string, float> floatMap;
     std::unordered_map<std::string, long> longMap;
 }Maps;
 
 struct{
-    std::regex varNameFirstLetter = std::regex("[A-Za-z]"); 
-    std::regex varNameNonFirstLetter = std::regex("[A-Za-z0-9]");
-    std::regex operators = std::regex("^(\\/=|\\+=|\\-=|\\*=|=)$");
+    std::regex varName = std::regex("^[A-Za-z][A-Za-z0-9]*$");
+    std::regex operators = std::regex("^(\\/=|\\+=|\\-=|\\*=|\\/|\\+|\\-|\\*)$");
+    std::regex longFloatRegex = std::regex("^[0-9]+\\.[0-9]+L;$");
+    std::regex floatRegex = std::regex("^[0-9]+\\.[0-9]+;$");
+    std::regex integerRegex = std::regex("^[0-9]+;$");
+
 }RegExes;
 
-void savevar(){}
-
-int vardec(string &token, char c){
-    string s(1, c);
-    if(token.length() == 0){
-        if(regex_search(s, RegExes.operators)){
-            cout << "You must declare var before assigning value!" << endl;
-            return -1;
-        }
-        if(!regex_search(s, RegExes.varNameFirstLetter)){
-            cout << "Incorrect var name format!" << endl;
-            return -2;
-        }
-        else{
-            token += c;
-        }
-    }
-    else{
-        if(!regex_search(s, RegExes.operators)){
-            return -3;
-        }
-        if(!regex_search(s, RegExes.varNameNonFirstLetter)){
-            cout << "Incorrect var name format or operation!" << endl;
-            return -2;
-        }
-        else{
-            token += c;
-        }
-    }
-    return 0;
+std::string shift(std::vector<std::string> &src)
+{
+    std::string current = src.front();
+    src.erase(src.begin());
+    return current;
 }
 
-int printVal(string &varToken){
-    string normalizedToken;
-    int len = varToken.length();
-    if(len == 0){
-        return -1;
-    }
-    for(int i = 0; i < len; i++){
-        if(varToken[i] == ' ') continue;
-        else { normalizedToken += varToken[i]; }
-    }
-    auto it = Maps.dataTypeMap.find(normalizedToken);
-    if (it == Maps.dataTypeMap.end()) {
-        return -2;
+std::vector<std::string> splitString(const std::string &sourceCode) {
+    std::vector<std::string> words;
+    std::string word;
+
+    for (char ch : sourceCode) {
+        if (ch != ' ') {
+            word += ch;
+        }
+        else if(ch == '[' || ch == ']' || ch == '{' || ch == '}'){
+            words.push_back(word);
+            word.clear();
+        }
+        else if (!word.empty()) {
+            words.push_back(word);
+            word.clear();
+        }
+        else{}
     }
 
-    switch (it->second) {
-        case tokenType::INT:
-            std::cout << Maps.intMap[normalizedToken] << std::endl;
-            break;
-        case tokenType::FLOAT:
-            std::cout << Maps.floatMap[normalizedToken] << std::endl;
-            break;
-        case tokenType::LONG:
-            std::cout << Maps.longMap[normalizedToken] << std::endl;
-            break;
-        default:
-            return -3;
+    if (!word.empty()) {
+        words.push_back(word);
     }
-    return 0;
+
+    return words;
 }
 
-int lexer(string &tokensLine){
-    int len = tokensLine.length();
-    tokenType dataType = INT;
-    tokenType currentExpected = VARDEC;
-    string currentToken = "";
-    int col = 0;
-    for(int i = 0; i < len; i++){
-        //if(tokensLine[i] == ' ') { continue; }
-        if(currentExpected == VARDEC){
-            if(vardec(currentToken, tokensLine[i]) == -3){
-                savevar(currentToken)
-            };
+std::vector<Token> tokenize(std::string &sourceCode) {
+    std::vector<Token> tokens;
+    std::vector<std::string> src = splitString(sourceCode);
+
+    while (!src.empty()) {
+        if(src.front() == "="){
+            tokens.push_back({shift(src), TokenType::Equals});
         }
-        if(currentExpected = VARDEF){
-
+        if (src.front() == "[") {
+            tokens.push_back({shift(src), TokenType::OpenBracket});
         }
-        if(i == len - 1 && currentExpected == VARDEC){
-            int response = 1;
-            response = printVal(currentToken);
-            switch(response){
-                case -1:
-
-                    break;
-                case -2:
-
-                    break;
-                case -3:
-
-                    break;
-            }
+        else if (src.front() == "]") {
+            tokens.push_back({shift(src), TokenType::CloseBracket});
         }
-        col++;
+        else if (src.front() == "{") {
+            tokens.push_back({shift(src), TokenType::OpenCurly});
+        }
+        else if (src.front() == "}") {
+            tokens.push_back({shift(src), TokenType::CloseCurly});
+        }
+        else if(src.front() == ";"){
+            tokens.push_back({shift(src), TokenType::Delimiter});
+        }
+        else if(regex_search(src.front(), RegExes.operators)){
+            tokens.push_back({shift(src), TokenType::Operator});
+        }
+        else if(regex_search(src.front(), RegExes.varName)){
+            tokens.push_back({shift(src), TokenType::Identifier});
+        }
+        else if(regex_search(src.front(), RegExes.longFloatRegex)){
+            tokens.push_back({shift(src), TokenType::Long});
+        }
+        else if(regex_search(src.front(), RegExes.floatRegex)){
+            tokens.push_back({shift(src), TokenType::Float});
+        }
+        else if(regex_search(src.front(), RegExes.integerRegex)){
+            tokens.push_back({shift(src), TokenType::Integer});
+        }
+        else{
+            tokens.push_back({shift(src), TokenType::Invalid});
+        }
     }
-    return 0;
+
+    return tokens;
 }
 
 int main(){
-    string currentTokens;
-    
+    std::string currentLine;
     while(true){
-        getline(cin, currentTokens); 
-        if (currentTokens.empty()) continue; 
-        if(lexer(currentTokens) == 1) exit(-1);
+        getline(cin, currentLine);
+        std::vector<Token> tokens = tokenize(currentLine);
+        for(int i = 0; i < tokens.size(); i++){
+            cout << "Token value: " << tokens[i].value << ", type: " << tokenTypeIdToString(tokens[i].type) << endl;
+        }
     }
     return 0; 
 }
